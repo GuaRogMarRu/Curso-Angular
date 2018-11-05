0121567028ce47e7a9e01c31974248e10939119b
RogelioMares <grmares@grupoeducare.com>
2018-11-05 13:58:01 -0600

    FEAT: hola mundo

--- 01-hola-mundo/src/app/app.component.ts
+++ 01-hola-mundo/src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  nombre:String = 'Rogelio';
+  apellido:String = 'Mares';
+  edad:number = 23;
 }
