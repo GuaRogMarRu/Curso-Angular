3324d78aef91505587503783106f955a2a727dfa
RogelioMares <grmares@grupoeducare.com>
2018-11-06 16:03:28 -0600

    FIX: SPA rutas

--- 02-SPA/src/app/servicios/heroes.service.ts
+++ 02-SPA/src/app/servicios/heroes.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class HeroesService {
 
-  private heroes:Heroes[] = [
+  private heroes:Heroe[] = [
       {
         nombre: "Aquaman",
         bio: "El poder más reconocido de Aquaman es la capacidad telepática para comunicarse con la vida marina, la cual puede convocar a grandes distancias.",
@@ -59,13 +59,12 @@ export class HeroesService {
     console.log("Servicio listo para usuar!!!");
   }
 
-  getHeroes():Heroes[]{
-
+  getHeroes():Heroe[]{
     return this.heroes;
   }
 }
 
-export interface Heroes{
+export interface Heroe{
   nombre: string;
   bio: string;
   img: string;
