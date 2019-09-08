ce7617294ba5e929833b6df70a5b42a406cc34b4
GuaRogMarRu <berserker.rojo123@gmail.com>
2019-09-08 15:13:53 -0500

    FEAT spotiapp: se agrega archivo de rutas

--- 04-spotiapp/src/app/app.module.ts
+++ 04-spotiapp/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,6 +8,10 @@ import { HomeComponent } from './components/home/home.component';
 import { SearchComponent } from './components/search/search.component';
 import { ArtistaComponent } from './components/artista/artista.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
+
+//Importar rutas
+import { ROUTES } from './app.routes';
+
 
 @NgModule({
   declarations: [
@@ -18,6 +23,7 @@ import { NavbarComponent } from './components/shared/navbar/navbar.component';
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot( ROUTES, { useHash: true }),
     AppRoutingModule
   ],
   providers: [],
